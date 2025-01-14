5274978516e66128371622aa7cf78409bc905feb
peter88213 <petert88213@gmail.com>
2025-01-14 22:45:40 +0100

    Version 5.0.2
    - Fix import.
    
    Based on novelibre 5.9.1

--- src/novx_xtg_.py
+++ src/novx_xtg_.py
@@ -10,7 +10,7 @@ import os
 from pathlib import Path
 import sys
 
-from mvclib.user_interface.ui_tk import UiTk
+from nvlib.gui.user_interface.ui_tk import UiTk
 from mvclib.view.set_icon_tk import set_icon
 from mvclib.view.ui import Ui
 from nvxtglib.xtg_config import XtgConfig

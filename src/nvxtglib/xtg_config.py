4b87a08bc42530ff4227e3031187590549439ba1
peter88213 <petert88213@gmail.com>
2026-01-12 13:11:04 +0100

    Refactored the configuration class.

--- src/nvxtglib/xtg_config.py
+++ src/nvxtglib/xtg_config.py
@@ -4,18 +4,16 @@ Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/novx_xtg
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
-import os
+from configparser import ConfigParser
 import json
-from nvlib.configuration.configuration import Configuration
+import os
 
 
-class XtgConfig(Configuration):
+class XtgConfig:
     """Read/write the program configuration.
     """
 
     def __init__(self, settings={}, options={}, templates={}, lookup={}):
-        """Overrides the superclass constructor, redefining _sLabel.
-        """
         self.templates = None
         self.lookup = None
         self._sLabel = 'STYLES'
@@ -23,8 +21,6 @@ class XtgConfig(Configuration):
         self.set(settings, options, templates, lookup)
 
     def set(self, settings=None, options=None, templates=None, lookup=None):
-        """Overrides the superclass method.
-        """
         if settings is not None:
             self.settings = settings.copy()
         if options is not None:
@@ -36,10 +32,24 @@ class XtgConfig(Configuration):
 
     def read(self, iniFile):
         """Read a configuration file.
+        
+        Positional arguments:
+            iniFile: str -- path configuration file path.
+            
         Settings and options that can not be read in, remain unchanged.
-        Extends the superclass.
         """
-        super().read(iniFile)
+        config = ConfigParser()
+        config.read(iniFile, encoding='utf-8')
+        if config.has_section(self._sLabel):
+            section = config[self._sLabel]
+            for setting in self.settings:
+                fallback = self.settings[setting]
+                self.settings[setting] = section.get(setting, fallback)
+        if config.has_section(self._oLabel):
+            section = config[self._oLabel]
+            for option in self.options:
+                fallback = self.options[option]
+                self.options[option] = section.getboolean(option, fallback)
         iniPath = os.path.dirname(iniFile)
         for template in self.templates:
             try:
@@ -64,8 +74,28 @@ class XtgConfig(Configuration):
 
     def write(self, iniFile):
         """Save the configuration to iniFile.
+
+        Positional arguments:
+            iniFile: str -- path configuration file path.
         """
-        super().write(iniFile)
+        config = ConfigParser()
+        if self.settings:
+            config.add_section(self._sLabel)
+            for settingId in self.settings:
+                config.set(
+                    self._sLabel,
+                    settingId,
+                    str(self.settings[settingId]),
+                )
+        if self.options:
+            config.add_section(self._oLabel)
+            for settingId in self.options:
+                if self.options[settingId]:
+                    config.set(self._oLabel, settingId, 'Yes')
+                else:
+                    config.set(self._oLabel, settingId, 'No')
+        with open(iniFile, 'w', encoding='utf-8') as f:
+            config.write(f)
         iniPath = os.path.dirname(iniFile)
         for template in self.templates:
             with open(f'{iniPath}/{template}.XTG', 'w', encoding='utf-8') as f:

23dad461be4b8da9f0df44894b26d4093d752b89
peter88213 <petert88213@gmail.com>
2026-01-12 13:16:39 +0100

    Updated the copyright notice.

--- src/nvxtglib/xtg_file.py
+++ src/nvxtglib/xtg_file.py
@@ -1,6 +1,6 @@
 """Provide a class for XPress tagged file processing. 
 
-Copyright (c) 2025 Peter Triesberger
+Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/novx_xtg
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
